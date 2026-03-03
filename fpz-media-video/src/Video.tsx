e34b5a51ad0a2efa14a9ea54fbcc78a40fe79e78
Thimofej <thimo@timax.io>
2026-03-03 23:45:26 +0100

    feat(v2): wire all V2 scenes in Video.tsx – 900fr complete

--- fpz-media-video/src/Video.tsx
+++ fpz-media-video/src/Video.tsx
@@ -1,9 +1,34 @@
-import { AbsoluteFill } from "remotion";
+import { AbsoluteFill, Series } from "remotion";
 import { GrainOverlay } from "./components/GrainOverlay";
-import { C } from "./tokens";
+import { Scene1Hook } from "./components/Scene1Hook";
+import { Scene2Web } from "./components/Scene2Web";
+import { Scene3Media } from "./components/Scene3Media";
+import { Scene4Automation } from "./components/Scene4Automation";
+import { Scene5Manifesto } from "./components/Scene5Manifesto";
+import { Scene6Endcard } from "./components/Scene6Endcard";
 
 export const FPZVideo = () => (
-  <AbsoluteFill style={{ background: C.bg }}>
+  <AbsoluteFill>
+    <Series>
+      <Series.Sequence durationInFrames={90}>
+        <Scene1Hook />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={210}>
+        <Scene2Web />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={210}>
+        <Scene3Media />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={180}>
+        <Scene4Automation />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={120}>
+        <Scene5Manifesto />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={90}>
+        <Scene6Endcard />
+      </Series.Sequence>
+    </Series>
     <GrainOverlay />
   </AbsoluteFill>
 );
