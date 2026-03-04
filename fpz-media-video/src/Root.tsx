38a128de0f191449a089f5c180e08e614c65c138
Thimofej <thimo@timax.io>
2026-03-04 23:16:10 +0100

    feat(video): add FPZCinematic composition (V1 30s dark)

--- fpz-media-video/src/Root.tsx
+++ fpz-media-video/src/Root.tsx
@@ -1,3 +1,16 @@
 import React from "react";
+import { Composition } from "remotion";
+import { VideoC1 } from "./VideoC1";
 
-export const RemotionRoot = (): React.ReactElement => <></>;
+export const RemotionRoot = (): React.ReactElement => (
+  <>
+    <Composition
+      id="FPZCinematic"
+      component={VideoC1}
+      durationInFrames={1800}
+      fps={60}
+      width={1920}
+      height={1080}
+    />
+  </>
+);
